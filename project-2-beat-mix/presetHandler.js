b4949a0be261b88510497bfd7e8b1fd54de6e581
Michael Zhou <michael.zhouzzy@gmail.com>
2018-04-29 23:16:57 -0700

    finish presetHandler.js Basic Functions

--- project-2-beat-mix/presetHandler.js
+++ project-2-beat-mix/presetHandler.js
@@ -2,7 +2,21 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (requestType, index, newPresetArray) => {
+    if(index < 0 || index >= preset.length){
+        return [404,[]];
+    }
+    if(requestType === 'PUT'){
+        preset[index] = newPresetArray;
+        return [200,preset[index]];
+    }
+    else if(requestType === 'GET'){
+        return [200,preset[index]];
+    }
+    else{
+        return [400,[]];
+    }
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
